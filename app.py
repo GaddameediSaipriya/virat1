efc08296b3218da548b749615f9499d8ffc5eca9
GaddameediSaipriya <113419037+GaddameediSaipriya@users.noreply.github.com>
2022-09-13 20:33:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 model = joblib.load('virat')
 st.title('Virat Centuries')
 input = st.text_input('enter the score and Batting order')
-output = model.predict([input])
+output = model.Predict([input])
 if st.button('predict'):
   st.title(output[0])
        
